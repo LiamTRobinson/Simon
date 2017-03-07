3329354312f94ad9f79ed0c9dedd258cdb5a05ae
LiamTRobinson <Liamtrobinson@gmail.com>
2017-03-07 12:42:17 -0500

    make game data object

--- main.js
+++ main.js
@@ -1,3 +1,49 @@
 const GameData = {
-	
-}
+	randomSequence: [],
+	userSequence: [],
+	randomSequenceRunning: false;
+	gameOn: false;
+	successfulSequences: 0,
+	buttonIds: ["button-1", "button-2", "button-3", "button-4"],
+
+	addToRandom: function() {
+		var randomNumber = Math.floor(Math.random() * 4) + 1;
+		this.randomSequence.push(randomNumber);
+	},
+	addToUser: function(buttonNum) {
+		this.userSequence.push(buttonNum);
+	},
+	increaseSuccessful: function() {
+		this.successfulSequences += 1;
+	},
+	startGame: function() {
+		if (this.gameOn === false) {
+			this.gameOn = true;
+		}
+	},
+	endGame: function() {
+		if (this.gameOn === true) {
+			this.gameOn = false;
+		}
+	},
+	startRandom: function() {
+		if (this.randomSequenceRunning === false) {
+			this.randomSequenceRunning = true;
+		}
+	},
+	endRandom: function() {
+		if (this.randomSequenceRunning === true) {
+			this.randomSequenceRunning = false;
+		}
+	},
+	randomClear: function() {
+		this.randomSequence = [];
+	},
+	userClear: function() {
+		this.userSequence = [];
+	},
+	clearSuccessful: function() {
+		this.successfulSequences = 0;
+	}
+};
+
